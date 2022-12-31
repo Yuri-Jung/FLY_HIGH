c21224a3da3f3db5eaf69b4ce3c12f6bc6ab1788
Yuri-Jung <dbfl14433@gmail.com>
2022-12-31 17:00:44 +0900

    ui 로그인

---
+++ src/main/java/com/fly/flyhigh/controller/controller.java
@@ -0,0 +1,37 @@
+package com.fly.flyhigh.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class controller {
+
+    @RequestMapping("/")
+    public String index() throws Exception {
+        return "index";
+    }
+
+    @RequestMapping("/header")
+    public String header() throws Exception {
+        return "header";
+    }
+    @RequestMapping("/footer")
+    public String footer() throws Exception {
+        return "footer";
+    }
+
+    @RequestMapping("/login")
+    public String login() throws Exception {
+        return "login";
+    }
+
+    @RequestMapping("/main")
+    public String main() throws Exception {
+        return "main";
+    }
+
+    @RequestMapping("register")
+    public String register() throws Exception {
+        return "register";
+    }
+}
